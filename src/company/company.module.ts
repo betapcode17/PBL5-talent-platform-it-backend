0ff9e3014eab1dd5b919b603bb7ab133d1b8a0cb
BachTran111 <tranxuanbach1029@gmail.com>
2026-03-26 20:13:22 +0700

    feat(company): implement company's dto/entity/module

---
+++ src/company/company.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { CompanyService } from './company.service.js';
+import { CompanyController } from './company.controller.js';
+import { PrismaService } from '../prisma.service.js';
+import { JwtAuthGuard } from '../jwt/jwt-auth.guard.js';
+import { MailsModule } from '../mails/mails.module.js';
+
+@Module({
+  imports: [MailsModule],
+  controllers: [CompanyController],
+  providers: [CompanyService, PrismaService, JwtAuthGuard],
+  exports: [CompanyService],
+})
+export class CompanyModule {}
